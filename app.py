8712a83c0e0e0e7a151a9688ea98f6c212ff88bc
Kshitiz Pandya <110394695+KshitizPandya@users.noreply.github.com>
2023-03-15 19:30:06 +0530

    This is the fastAPI for direct text translation.

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from googletrans import Translator
+
+app = FastAPI()
+
+class RequestModel(BaseModel):
+    text: str
+    language: str
+
+class ResponseModel(BaseModel):
+    translation: str
+
+def translate_text(text, target_language):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+
+def get_language_code(language):
+    indic_language_codes = {"english": "en", "hindi": "hi", "gujarati": "gu", "arabic": "ar", "assamese": "as",
+                            "bengali": "bn", "kannada": "kn", "malayalam": "ml", "marathi": "mr", "nepali": "ne",
+                            "punjabi": "pa", "tamil": "ta", "telugu": "te", "thai": "th", "urdu": "ur",
+                            "vietnamese": "vi"}
+
+    foreign_language_codes = {"french": "fr", "german": "de", "italian": "it", "japanese": "ja", "korean": "ko",
+                              "portuguese": "pt", "russian": "ru", "spanish": "es", "turkish": "tr"}
+
+    language_code = indic_language_codes.get(language.lower())
+    if not language_code:
+        language_code = foreign_language_codes.get(language.lower())
+    if not language_code:
+        print(f"Error: Language {language} not supported.")
+    return language_code
+
+@app.post("/translate")
+def extract_and_translate(request: RequestModel) -> ResponseModel:
+    text = request.text
+    target_language = request.language.lower()
+
+    language_code = get_language_code(target_language)
+
+    if not language_code:
+        return {"translation": "Error: Language not supported."}
+
+    translation = translate_text(text, language_code)
+
+    if not translation:
+        return {"translation": "Error: Translation failed."}
+
+    return {"translation": translation}
