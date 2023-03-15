c1d788652f76496d25a0c8e3794cc17d33f682fa
Kshitiz Pandya <110394695+KshitizPandya@users.noreply.github.com>
2023-03-15 21:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ class ResponseModel(BaseModel):
     translation: str
 
 def translate_text(text, target_language):
-    print("This is the text feeded for translation: ", text, type(text))
-    print("This is the target_language feeded for translation: ", target_language, type(target_language))
+#     print("This is the text feeded for translation: ", text, type(text))
+#     print("This is the target_language feeded for translation: ", target_language, type(target_language))
     translator = Translator()
     try:
         translation = translator.translate(text, dest=target_language)
@@ -22,7 +22,7 @@ def translate_text(text, target_language):
         print(f"Error: {e}")
         return None
 
-    print("HIHIHIHIHIHIHI_1: ", translation.text)
+#     print("HIHIHIHIHIHIHI_1: ", translation.text)
     return translation.text
 
 def get_language_code(language):
@@ -52,7 +52,7 @@ def extract_and_translate(request: RequestModel) -> ResponseModel:
         return {"translation": "Error: Language not supported."}
 
     translation = translate_text(text, language_code)
-    print("HIHIHIHIHIHIHI_2: ", translation)
+#     print("HIHIHIHIHIHIHI_2: ", translation)
     
     if not translation:
         return {"translation": "Error: Translation failed."}
